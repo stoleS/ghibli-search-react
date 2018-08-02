b56333dcb8eddcc91e39e2a1aa905b0697a61444
stoleS <white.knight0857@gmail.com>
2018-08-02 13:49:20 +0200

    Style changes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 import "../App.css";
-import { Col, Card, CardImg, CardFooter } from "reactstrap";
+import { Col, Card, CardImg, CardBody, CardText, Badge } from "reactstrap";
 
 class Movie extends Component {
   render() {
     const title = this.props.movie.title;
+    const score = this.props.movie.rt_score;
+    const releaseDate = this.props.movie.release_date;
+    const director = this.props.movie.director;
     return (
-      <Col md="3" sm="4" xs="6" className="mb-5">
-        <Card className="h-100 shadow">
+      <Col md="3" sm="4" xs="6" className="mb-3 card-container rounded">
+        <Card className="h-100">
           <CardImg
             top
             width="100%"
             src={this.props.movie.image}
             alt="Card image cap"
+            className="shadow-sm rounded"
           />
-          <CardFooter className="text-muted">{title}</CardFooter>
+          <Badge color="dark" pill className="rating-badge">
+            {score}
+          </Badge>
+          <CardBody>
+            <CardText className="movie-title">{title}</CardText>
+            <CardText className="movie-title">
+              {releaseDate}, {director}
+            </CardText>
+          </CardBody>
         </Card>
       </Col>
     );
